d9ea09a095b4eca89be19df9c02f8de150eff0c9
Arthur E. Jones <PartyLich@gmail.com>
2019-09-24 20:45:47 -0400

    feat: add graphics system draw()

--- space-invaders/src/systems/graphics.js
+++ space-invaders/src/systems/graphics.js
@@ -10,8 +10,27 @@ const Graphics = function (gameSize) {
   const clearScreen = (screen) =>
     screen.clearRect(0, 0, gameSize.x, gameSize.y);
 
+  /**
+   * draws passed body as a rectangle to `screen`, the drawing context.
+   * @param  {CanvasRenderingContext2D} screen the drawing context
+   * @param  {object} entity   drawable object
+   */
+  const draw = (screen) => (entity) => {
+    if (entity.isDrawable) {
+      screen.fillRect(
+          entity.center.x - entity.size.x / 2,
+          entity.center.y - entity.size.y / 2,
+          entity.size.x,
+          entity.size.y
+      );
+    }
+
+    return entity;
+  };
+
   return {
     clearScreen,
+    draw,
   };
 };
 
