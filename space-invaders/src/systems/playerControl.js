1c3bfb7ed32c6b9789cef22a256db1a6ec4afef8
Arthur E. Jones <PartyLich@gmail.com>
2019-09-24 23:08:46 -0400

    feat: add playerControl fire()

--- space-invaders/src/systems/playerControl.js
+++ space-invaders/src/systems/playerControl.js
@@ -1,4 +1,5 @@
 import { pipe } from '../lib/lib';
+import createBullet from '../entities/bullet';
 
 const PlayerControl = (entityManager, keyboarder, shootSound) => {
   /**
@@ -23,10 +24,48 @@ const PlayerControl = (entityManager, keyboarder, shootSound) => {
   }
 
   /**
+   * Play shooting audio
+   */
+  function playShootSound() {
+    // ... rewind the shoot sound...
+    shootSound.load();
+
+    // ... and play the shoot sound.
+    shootSound.play();
+  }
+
+  /**
+   * Respond to player shoot input
+   * @param  {object} entity
+   * @return {object}
+   */
+  function fire(entity) {
+    if (entity.playerControlled) {
+      // If S key is down...
+      if (keyboarder.isDown(keyboarder.KEYS.S)) {
+        // ... create a bullet just above the player that will move upwards...
+        const bulletCenter = {
+          x: entity.center.x,
+          y: entity.center.y - entity.size.y - 10,
+        };
+        const bulletVelocity = { x: 0, y: -7 };
+        const bullet = createBullet(bulletCenter, bulletVelocity);
+
+        // ... add the bullet to the game...
+        entityManager.add(bullet);
+
+        playShootSound();
+      }
+    }
+    return entity;
+  }
+
+  /**
    * updates the state of the player for a single tick.
    */
   const simulate = pipe(
-      move
+      move,
+      fire
   );
 
   return {
