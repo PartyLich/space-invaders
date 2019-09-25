bf45810090595bf4e8e7693af618d0d9b64f6600
Arthur E. Jones <PartyLich@gmail.com>
2019-09-24 21:36:44 -0400

    feat: create invader entities in game

--- space-invaders/src/systems/engine.js
+++ space-invaders/src/systems/engine.js
@@ -5,6 +5,8 @@ import { pipe } from '../lib/lib';
 import createEntityManager from './entityManager';
 import createGraphics from './graphics';
 
+import createInvader from '../entities/invader';
+
 /**
  * Get the drawing context.  This contains functions that let you draw
  * to the canvas.
@@ -14,10 +16,36 @@ const getScreen = () =>
   document.getElementById('space-invaders').getContext('2d');
 
 /**
+ * returns an array of invaderCount invaders.
+ * @param {number} invaderCount number of invaders to create
+ * @return {array} an array of invaders
+ */
+const createInvaders = (invaderCount) => {
+  const invaders = [];
+
+  for (let i = 0; i < invaderCount; i++) {
+    // Place invaders in eight columns.
+    const x = 10 + (i % 8) * 30;
+
+    // Place invaders in three rows.
+    const y = 30 + (i % 3) * 30;
+
+    // Create invader.
+    const invader = createInvader({ x, y });
+
+    // Add invader to result array
+    invaders.push(invader);
+  }
+
+  return invaders;
+};
+
+/**
  * Creates the game object with the game state and logic.
  * @return {object}
  */
 const Engine = function () {
+  const INVADER_COUNT = 24;
   // In index.html, there is a canvas tag that the game will be drawn in.
   // Grab that canvas out of the DOM.
   const canvas = document.getElementById('space-invaders');
@@ -36,6 +64,10 @@ const Engine = function () {
   // Create the entity manager to hold the player, invaders, bullets etc
   const entityManager = createEntityManager();
   const graphics = createGraphics(gameSize);
+
+  // Entities contain all of the data that systems operate upon.
+  // Add the invaders to the entity manager
+  entityManager.add(createInvaders(INVADER_COUNT));
 
   // Simulation processing pipeline.
   const simulate = pipe(
