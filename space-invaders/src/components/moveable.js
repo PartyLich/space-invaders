80cfe1bbb32950bc431881b39e8302e26ee1c763
Arthur E. Jones <PartyLich@gmail.com>
2019-09-25 01:25:59 -0400

    fix: fix moveable default parameters

--- space-invaders/src/components/moveable.js
+++ space-invaders/src/components/moveable.js
@@ -1,7 +1,11 @@
-import createCoord from './coord';
-
-const createMoveable = (velocity = createCoord()) => ({
-  velocity,
+/**
+ * @param {object} velocity
+ * @param  {number} velocity.x
+ * @param  {number} velocity.y
+ * @return {object}
+ */
+const createMoveable = ({ x = 0, y = 0 } = {}) => ({
+  velocity: { x, y },
   isMoveable: true,
 });
 
