0e33296eb57396d88bbfa7f497dddb2dba6c666c
Arthur E. Jones <PartyLich@gmail.com>
2019-09-24 22:25:18 -0400

    feat: add invaderAi fire()

--- space-invaders/src/systems/invaderAi.js
+++ space-invaders/src/systems/invaderAi.js
@@ -1,3 +1,5 @@
+import Bullet from '../entities/bullet';
+
 const invaderAi = (entityManager) => {
   /**
    * Update entity patrol path
@@ -17,6 +19,59 @@ const invaderAi = (entityManager) => {
   }
 
   /**
+   * returns true if `invader` is directly above at least one other invader.
+   * @param  {object} invader
+   * @param  {number} i
+   * @param {array} entities
+   * @return {boolean}
+   */
+  const invadersBelow = function (invader, i, entities) {
+    // If filtered array is not empty, there are invaders below.
+    return (
+      entities.filter((b) =>
+        // Keep `b` if it is an invader, if it is in the same column
+        // as `invader`, and if it is somewhere below `invader`.
+        (
+          b.aiControlled &&
+          Math.abs(invader.center.x - b.center.x) < b.size.x &&
+          b.center.y > invader.center.y
+        )
+      ).length > 0
+    );
+  };
+
+
+  /**
+   * Fire a bullet
+   * @param  {object} entity
+   * @param {number} i
+   * @param  {array} entityList
+   */
+  function fire(entity, i) {
+    const entityArr = entityManager.entityArray()
+    i = i || entityArr.indexOf(entity);
+    const shotRoll = Math.random() > 0.995;
+    const clearSight = shotRoll &&
+        !invadersBelow(entity, i, entityArr);
+
+    // If coin flip comes up and no friends below in this
+    // invader's column...
+    if (clearSight) {
+      // ... create a bullet just below the invader that will move
+      // downward...
+      const bulletCenter = {
+        x: entity.center.x,
+        y: entity.center.y + entity.size.y / 2,
+      };
+      const bulletVelocity = { x: 0, y: 2 };
+      const bullet = Bullet(bulletCenter, bulletVelocity);
+
+      // ... and add the bullet to the game.
+      entityManager.add(bullet);
+    }
+  }
+
+  /**
    * Simulate
    * @param  {object} entity
    * @param  {number} i
@@ -24,6 +79,7 @@ const invaderAi = (entityManager) => {
    */
   function simulate(entity, i) {
     updatePatrol(entity);
+    fire(entity, i);
 
     return entity;
   }
